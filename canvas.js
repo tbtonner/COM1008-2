3be650321d1888ce4dde52d9ac39da86a4dad71a
AquinasAhoy <44973989+AquinasAhoy@users.noreply.github.com>
2018-12-02 17:13:06 +0000

    tutle move to click complete

--- canvas.js
+++ canvas.js
@@ -18,30 +18,55 @@ var mouse = {
 var imgWidth = 169;
 var imgHeight = 237;
 
+//Request to stop ID
+var requestId;
+
 //Setting initial position of the turtle
 var xPos = (1/2 * canvas.width) - 1/2*imgWidth;
 var yPos = (1/2 * canvas.height) - 1/2*imgHeight;
 
-//Variables of the line between current position and mouse click
-var m = (yPos-mouse.y)/(xPos-mouse.x);
-var c = yPos-m*xPos;
+//Array for all coordinates
 var coords = [];
 
 //Event listener when mouse moves
 window.addEventListener("click", function(event){
     coords = [];
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
     mouse.x = event.x - 1/2*imgWidth;
     mouse.y = event.y- 1/2*imgHeight;
 
-    for (var xCurrent = xPos; xCurrent < mouse.x; xCurrent++) {
-        var yCurrent = m*xCurrent + c;
-        storeCoordinate(xCurrent, yCurrent, coords);
+    //Variables of the line between current position and mouse click
+    var m = (yPos-mouse.y)/(xPos-mouse.x);
+    var c = yPos-m*xPos;
+
+    var speed = 3;
+
+    if (Math.abs(yPos-mouse.y)>Math.abs(xPos-mouse.x)) {
+        if (mouse.y > yPos) {
+            for (var yCurrent = yPos; yCurrent < mouse.y; yCurrent=yCurrent+speed) {
+                var xCurrent = (yCurrent - c)/m;
+                storeCoordinate(xCurrent, yCurrent, coords);
+            }
+        }else{
+            for (var yCurrent = yPos; yCurrent > mouse.y; yCurrent=yCurrent-speed) {
+                var xCurrent = (yCurrent - c)/m;
+                storeCoordinate(xCurrent, yCurrent, coords);
+            }
+        }
+    }else{
+        if (mouse.x > xPos) {
+            for (var xCurrent = xPos; xCurrent < mouse.x; xCurrent=xCurrent+speed) {
+                var yCurrent = m*xCurrent + c;
+                storeCoordinate(xCurrent, yCurrent, coords);
+            }
+        }else{
+            for (var xCurrent = xPos; xCurrent > mouse.x; xCurrent=xCurrent-speed) {
+                var yCurrent = m*xCurrent + c;
+                storeCoordinate(xCurrent, yCurrent, coords);
+            }
+        }
     }
 
-    animate();
+    nextFrame(0);
 
 });
 
@@ -60,23 +85,42 @@ function storeCoordinate(xVal, yVal, array) {
     });
 }
 
+function stop() {
+    if (requestId){
+        cancelAnimationFrame(requestId);
+    }
+}
+
+var img = new Image();
+
+function translate(x, y){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.save();
+    context.drawImage(img, x, y);
+    context.restore();
+}
+
 function draw(x, y){
-    var img = new Image();
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.save();
     img.onload = function() {
         context.drawImage(img, x, y);
     }
     img.src = "./turtle1.png";
 }
 
-function animate(){
-    requestAnimationFrame(animate);
-    // context.clearRect(0, 0, canvas.width, canvas.height);
+function nextFrame(i) {
+    xPos = coords[i].x
+    yPos = coords[i].y;
+    translate(coords[i].x, coords[i].y);
+    requestId = requestAnimationFrame(function() {
+        nextFrame(i);
+    });
 
-    for (var i = 0; i < coords.length; i++) {
-        draw(coords[i].x, coords[i].y);
-        if (i == coords.length){
-            cancelAnimationFrame(animate);
-        }
+    i++;
+    if (i == coords.length) {
+        stop();
     }
 }
 
