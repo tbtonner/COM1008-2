c544ef3b974f2ec7f4ac68558fadb0f7d95eeef4
Tom <agcdoctor@gmail.com>
2018-12-08 22:41:57 +0000

    comment more

--- canvas.js
+++ canvas.js
@@ -84,7 +84,7 @@ window.addEventListener("resize", function(){
     btnReset(); //Reset all parts of canvas
 });
 
-//--------------------Simple functions--------------------//
+//--------------------Small functions--------------------//
 
 function resetArrays(){ //Function that resets all arrays to []
     coords = [];
@@ -98,15 +98,15 @@ function resetArrays(){ //Function that resets all arrays to []
 function loadImages() { //Initial function to load all images
     for (var i=0; i<imgSource.length; i++) {
         var image = new Image();
-        image.onload = function () {
-            loadcount++;
+        image.onload = function () { //Loading images
+            loadcount++; //Incrementing image count
             if (loadcount == loadtotal) {
-                draw(xPos, yPos, currentRotation, 0, false, false, false);
+                draw(xPos, yPos, currentRotation, 0, false, false, false); //Initial drawing of turtle
             }
         };
 
         image.src = imgSource[i];
-        img[i] = image;
+        img[i] = image; //Storing images in an array
     }
 }
 
@@ -122,8 +122,9 @@ function storeCoordinate(xVal, yVal, array) { //Function to store coordinates of
 }
 
 function stop() { //Stop the animation frame
-    cancelAnimationFrame(requestId);
+    cancelAnimationFrame(requestId); //Stop the animation
 
+    //Change walking state if walking button pressed
     if (walking == 1) {
         walking++;
         walk(xPos + 200, yPos);
@@ -139,12 +140,223 @@ function stop() { //Stop the animation frame
     }
 }
 
+//--------------------Button functions--------------------//
+
+function btnReset(){ //Resets entire screen
+    //Resetting x and y positions
+    xPos = (1/2 * canvas.width) - 1/2*imgWidth;
+    yPos = (1/2 * canvas.height) - 1/2*imgHeight;
+    //Setting current rotation and current image index to 0
+    currentRotation = 0;
+    currentImageIndex = 0;
+    //Setting going in shell boolean to false
+    goingIntoShell = false;
+    //Setting walking and imgDiff to 0
+    walking = 0;
+    imgDiff = 0;
+
+    stop(); //Stops all animations
+    resetArrays(); //Resets all arrays to []
+
+    draw(xPos, yPos, currentRotation, 0, false, false, false); // Draw initial position
+}
+
+function btnDance(){ //Makes the turtle dance
+    walking = 0; //Set walking state to 0
+    stop(); //Stoping all animations
+    resetArrays(); //Resetng all arrays
+
+    if (currentImageIndex == 11 || currentImageIndex == 12) { //If image is laser turtle or sleeping turtle then:
+        draw(xPos, yPos, currentRotation, 0, false, false, false); //Draw normal turtle
+        currentImageIndex = 0;
+    }
+
+    var division = 50; //Setting division for rotation
+
+    //Make turtle come out of shell if he is in it or partially in it
+    if (currentImageIndex != 0) {
+        goingIntoShell = false;
+        for (var i = currentImageIndex; i >= 0; i--) {
+            for (var t = 0; t < 5; t++) {
+                imageIndex.push(i);
+                rotate.push(currentRotation);
+                storeCoordinate(xPos, yPos, coords);
+            }
+        }
+    }
+
+    var clockwise = false; //Clockwise is equal to false for now
+
+    //Determining if turtle should roate clockwise of anticlockwise
+    for (var i = 0; i < 180; i++) {
+        if (modulo((currentRotation + i*(Math.PI/180)), (Math.PI*2)) <= maxRotate) {
+            if (modulo((currentRotation + (i+1)*(Math.PI/180)), (Math.PI*2)) >= maxRotate) {
+                clockwise = true;
+            }
+        }
+    }
+
+    var diffAngle;
+
+    //Rotating turtle to inital rotation (0)
+    if (clockwise == true) {
+        diffAngle = modulo((0 - currentRotation), (Math.PI * 2));
+        for (var i = 0; i < division; i++) {
+            rotate.push(modulo((currentRotation + i*diffAngle/division), (Math.PI*2)));
+            storeCoordinate(xPos, yPos, coords);
+            imageIndex.push(0);
+        }
+    }else {
+        diffAngle = modulo((currentRotation - 0), (Math.PI * 2));
+        for (var i = 0; i < division; i++) {
+            rotate.push(modulo((currentRotation - i*diffAngle/division), (Math.PI*2)));
+            storeCoordinate(xPos, yPos, coords);
+            imageIndex.push(0);
+        }
+    }
+
+    //Clockwise fast circle roatation
+    for (var i = 0; i <= 60; i++) {
+        rotate.push(modulo((i*(Math.PI/180))*6, (Math.PI*2)));
+        storeCoordinate(xPos, yPos, coords);
+        imageIndex.push(0);
+    }
+
+    //Anti-clockwise fast circle roatation
+    for (var i = 0; i <= 60; i++) {
+        rotate.push(modulo((-i*(Math.PI/180))*6, (Math.PI*2)));
+        storeCoordinate(xPos, yPos, coords);
+        imageIndex.push(0);
+    }
+
+    var dis = 10;
+    var danceSpeed = 4;
+
+    //x
+    for (var i = 0; i < dis; i++) {
+        rotate.push(0);
+        storeCoordinate(xPos + i*danceSpeed, yPos, coords);
+        imageIndex.push(0);
+    }
+    for (var i = dis; i > -dis; i--) {
+        rotate.push(0);
+        storeCoordinate(xPos + i*danceSpeed, yPos, coords);
+        imageIndex.push(0);
+    }
+    for (var i = -dis; i < 0; i++) {
+        rotate.push(0);
+        storeCoordinate(xPos + i*danceSpeed, yPos, coords);
+        imageIndex.push(0);
+    }
+
+    //y
+    for (var i = 0; i < dis; i++) {
+        rotate.push(0);
+        storeCoordinate(xPos, yPos + i*danceSpeed, coords);
+        imageIndex.push(0);
+    }
+    for (var i = dis; i > -dis; i--) {
+        rotate.push(0);
+        storeCoordinate(xPos, yPos + i*danceSpeed, coords);
+        imageIndex.push(0);
+    }
+    for (var i = -dis; i < 0; i++) {
+        rotate.push(0);
+        storeCoordinate(xPos, yPos + i*danceSpeed, coords);
+        imageIndex.push(0);
+    }
+
+    //Clockwise fast circle roatation
+    for (var i = 0; i <= 60; i++) {
+        rotate.push(modulo((i*(Math.PI/180))*6, (Math.PI*2)));
+        storeCoordinate(xPos, yPos, coords);
+        imageIndex.push(0);
+    }
+
+    for (var i = 0; i < coords.length; i++) {
+        sleep1.push(false);
+        sleep2.push(false);
+        sleep3.push(false);
+    }
+
+    nextFrame(0);
+}
+
+function btnSleep(){ //Makes the turtle sleep
+    walking = 0;
+    stop();
+    resetArrays();
+
+    if (currentImageIndex == 11 || currentImageIndex == 12) {
+        imageIndex.push(0);
+        rotate.push(currentRotation);
+        storeCoordinate(xPos, yPos, coords);
+    }else{
+        if (currentImageIndex != 0) {
+            goingIntoShell = false;
+            for (var i = currentImageIndex; i >= 0; i--) {
+                for (var t = 0; t < 5; t++) {
+                    imageIndex.push(i);
+                    rotate.push(currentRotation);
+                    storeCoordinate(xPos, yPos, coords);
+                    sleep1.push(false);
+                    sleep2.push(false);
+                    sleep3.push(false);
+                }
+            }
+        }
+
+        for (var i = 0; i < 160; i++) {
+            imageIndex.push(11);
+            rotate.push(currentRotation);
+            storeCoordinate(xPos, yPos, coords);
+            if (i < 40) {
+
+                sleep1.push(false);
+                sleep2.push(false);
+                sleep3.push(false);
+            }else if (i < 80) {
+                sleep1.push(true);
+                sleep2.push(false);
+                sleep3.push(false);
+            }else if (i < 120) {
+                sleep1.push(true);
+                sleep2.push(true);
+                sleep3.push(false);
+            }else if (i < 160) {
+                sleep1.push(true);
+                sleep2.push(true);
+                sleep3.push(true);
+            }
+        }
+    }
+
+    nextFrame(0);
+}
+
+function btnWalk(){ //What happens when walk button is pressed
+    stop();
+    walking = 1;
+    walk(xPos - 100, yPos - 100);
+}
+
 //--------------------Large functions--------------------//
 
 function clickOnTurtleHead(){ //When clicking on turtle's head
     walking = 0;
     stop();
     resetArrays();
+
+    if (goingIntoShell) {
+        goingIntoShell = !goingIntoShell;
+        for (var i = currentImageIndex; i >= 0; i--) {
+            for (var t = 0; t < 5; t++) {
+                imageIndex.push(i);
+                rotate.push(currentRotation);
+                storeCoordinate(xPos, yPos, coords);
+            }
+        }
+    }
 
     //Clockwise fast circle roatation
     for (var i = 0; i <= 360/3; i++) {
@@ -325,197 +537,6 @@ function walk(toX, toY){ //Function to walk from current position to (toX, toY) 
     }
 
     nextFrame(0);
-}
-
-function btnReset(){ //Resets entire screen
-    xPos = (1/2 * canvas.width) - 1/2*imgWidth;
-    yPos = (1/2 * canvas.height) - 1/2*imgHeight;
-    currentRotation = 0;
-    currentImageIndex = 0;
-    goingIntoShell = false;
-    walking = 0;
-    imgDiff = 0;
-
-    stop();
-    resetArrays();
-
-    draw(xPos, yPos, currentRotation, 0, false, false, false);
-}
-
-function btnDance(){ //Makes the turtle dance
-    walking = 0;
-    stop();
-    resetArrays();
-
-    if (currentImageIndex == 11 || currentImageIndex == 12) {
-        draw(xPos, yPos, currentRotation, 0, false, false, false);
-        currentImageIndex = 0;
-    }
-
-    var division = 50;
-
-    if (currentImageIndex != 0) {
-        goingIntoShell = false;
-        for (var i = currentImageIndex; i >= 0; i--) {
-            for (var t = 0; t < 5; t++) {
-                imageIndex.push(i);
-                rotate.push(currentRotation);
-                storeCoordinate(xPos, yPos, coords);
-            }
-        }
-    }
-
-    var clockwise = false;
-
-    for (var i = 0; i < 180; i++) {
-        if (modulo((currentRotation + i*(Math.PI/180)), (Math.PI*2)) <= 0) {
-            if (modulo((currentRotation + (i+1)*(Math.PI/180)), (Math.PI*2)) >= 0) {
-                clockwise = true;
-            }
-        }
-    }
-
-    var diffAngle;
-
-    if (clockwise == true) {
-        diffAngle = modulo((0 - currentRotation), (Math.PI * 2));
-        for (var i = 0; i < division; i++) {
-            rotate.push(modulo((currentRotation + i*diffAngle/division), (Math.PI*2)));
-            storeCoordinate(xPos, yPos, coords);
-            imageIndex.push(0);
-        }
-    }else {
-        diffAngle = modulo((currentRotation - 0), (Math.PI * 2));
-        for (var i = 0; i < division; i++) {
-            rotate.push(modulo((currentRotation - i*diffAngle/division), (Math.PI*2)));
-            storeCoordinate(xPos, yPos, coords);
-            imageIndex.push(0);
-        }
-    }
-
-    //Clockwise fast circle roatation
-    for (var i = 0; i <= 60; i++) {
-        rotate.push(modulo((i*(Math.PI/180))*6, (Math.PI*2)));
-        storeCoordinate(xPos, yPos, coords);
-        imageIndex.push(0);
-    }
-
-    //Anti-clockwise fast circle roatation
-    for (var i = 0; i <= 60; i++) {
-        rotate.push(modulo((-i*(Math.PI/180))*6, (Math.PI*2)));
-        storeCoordinate(xPos, yPos, coords);
-        imageIndex.push(0);
-    }
-
-    var dis = 10;
-    var danceSpeed = 4;
-
-    //x
-    for (var i = 0; i < dis; i++) {
-        rotate.push(0);
-        storeCoordinate(xPos + i*danceSpeed, yPos, coords);
-        imageIndex.push(0);
-    }
-    for (var i = dis; i > -dis; i--) {
-        rotate.push(0);
-        storeCoordinate(xPos + i*danceSpeed, yPos, coords);
-        imageIndex.push(0);
-    }
-    for (var i = -dis; i < 0; i++) {
-        rotate.push(0);
-        storeCoordinate(xPos + i*danceSpeed, yPos, coords);
-        imageIndex.push(0);
-    }
-
-    //y
-    for (var i = 0; i < dis; i++) {
-        rotate.push(0);
-        storeCoordinate(xPos, yPos + i*danceSpeed, coords);
-        imageIndex.push(0);
-    }
-    for (var i = dis; i > -dis; i--) {
-        rotate.push(0);
-        storeCoordinate(xPos, yPos + i*danceSpeed, coords);
-        imageIndex.push(0);
-    }
-    for (var i = -dis; i < 0; i++) {
-        rotate.push(0);
-        storeCoordinate(xPos, yPos + i*danceSpeed, coords);
-        imageIndex.push(0);
-    }
-
-    //Clockwise fast circle roatation
-    for (var i = 0; i <= 60; i++) {
-        rotate.push(modulo((i*(Math.PI/180))*6, (Math.PI*2)));
-        storeCoordinate(xPos, yPos, coords);
-        imageIndex.push(0);
-    }
-
-    for (var i = 0; i < coords.length; i++) {
-        sleep1.push(false);
-        sleep2.push(false);
-        sleep3.push(false);
-    }
-
-    nextFrame(0);
-}
-
-function btnSleep(){ //Makes the turtle sleep
-    walking = 0;
-    stop();
-    resetArrays();
-
-    if (currentImageIndex == 11 || currentImageIndex == 12) {
-        imageIndex.push(0);
-        rotate.push(currentRotation);
-        storeCoordinate(xPos, yPos, coords);
-    }else{
-        if (currentImageIndex != 0) {
-            goingIntoShell = false;
-            for (var i = currentImageIndex; i >= 0; i--) {
-                for (var t = 0; t < 5; t++) {
-                    imageIndex.push(i);
-                    rotate.push(currentRotation);
-                    storeCoordinate(xPos, yPos, coords);
-                    sleep1.push(false);
-                    sleep2.push(false);
-                    sleep3.push(false);
-                }
-            }
-        }
-
-        for (var i = 0; i < 160; i++) {
-            imageIndex.push(11);
-            rotate.push(currentRotation);
-            storeCoordinate(xPos, yPos, coords);
-            if (i < 40) {
-
-                sleep1.push(false);
-                sleep2.push(false);
-                sleep3.push(false);
-            }else if (i < 80) {
-                sleep1.push(true);
-                sleep2.push(false);
-                sleep3.push(false);
-            }else if (i < 120) {
-                sleep1.push(true);
-                sleep2.push(true);
-                sleep3.push(false);
-            }else if (i < 160) {
-                sleep1.push(true);
-                sleep2.push(true);
-                sleep3.push(true);
-            }
-        }
-    }
-
-    nextFrame(0);
-}
-
-function btnWalk(){ //What happens when walk button is pressed
-    stop();
-    walking = 1;
-    walk(xPos - 100, yPos - 100);
 }
 
 function draw(x, y, rotation, imgIndex, sleep_1, sleep_2, sleep_3){ //Draw turtle and other items on screen with given peramters
